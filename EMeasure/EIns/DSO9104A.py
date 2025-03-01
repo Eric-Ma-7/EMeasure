fe4d032715bf5bc7becae9588b664c9bc4e24fc4
Eric-Ma-7 <39188334+Eric-Ma-7@users.noreply.github.com>
2025-03-01 20:37:14 +0800

    Add methods about trigger

--- EMeasure/EIns/DSO9104A.py
+++ EMeasure/EIns/DSO9104A.py
@@ -1,64 +1,79 @@
 from .EIns import EIns
 import numpy as np
 
+from typing import List, Dict
+
 
 class DSO9104A(EIns):
 
-    def __init__(self, resource_name):
+    def __init__(self, resource_name:str) -> None:
         super().__init__(resource_name)
     
-
-    def get_xy_info(self, channel):
-        self.write(f':waveform:source channel{channel}')
-        xy_info = {
-            'xOrg': self.query(':waveform:xorigin?'),
-            'xInc': self.query(':waveform:xincrement?'),
-            'yOrg': self.query(':waveform:yorigin?'),
-            'yInc': self.query(':waveform:yincrement?'),
-        }
+    
+    def query_value(self, command:str) -> float | None:
+        resp = self.query(command).strip()
         try:
-            xy_info = {key: float(val) for key,val in xy_info.items()}
-            return xy_info
+            return float(resp)
         except ValueError as e:
             print(f"Failed to convert response to float: {e}")
             return None
-
-
-    def get_waveform(self, channel):
-        self.write(':digitize')
-        self.write(':system:header off')
-        self.write(':waveform:byteorder LSBfirst')
-        self.write(':waveform:format ascii')
+    
+    
+    def _get_waveform(self, channel:int) -> dict:
+        self.write(f':waveform:source channel{channel}')
+        resp = self.query(':waveform:data?')
+        resp = resp.strip().rstrip(',')
+        
+        waveform = {
+            'xOrg': float(self.query(':waveform:xorigin?').strip()),
+            'xInc': float(self.query(':waveform:xincrement?').strip()),
+            'xUnits': self.query(':waveform:xunits?').strip(),
+            'yOrg': float(self.query(':waveform:yorigin?').strip()),
+            'yInc': float(self.query(':waveform:yincrement?').strip()),
+            'yUnits': self.query(':waveform:yunits?').strip(),
+            'yData': [float(s) for s in resp.split(',')]
+        }
+        
+        return waveform
+        
+    
+    
+    def capture_waveform(self, channel:int|List[int]):
         self.write(':waveform:streaming off')
-
-        channel = channel if isinstance(channel,list) else [channel]
-        y_datas = []
-        for ch in channel:
-            self.write(f':waveform:source channel{ch}')
-            resp = self.query(':waveform:data?')
-            resp = resp.strip().rstrip(',')
-            try:
-                y_data = [float(s) for s in resp.split(',')]
-            except ValueError as e:
-                print(f"Failed to convert response to float: {e}")
-                y_data = None
-            y_datas.append(y_data)
-
-        self.write(':waveform:streaming on')
-        self.write(':run')
-
-        return y_datas
+        self.write(':waveform:format ascii')
+        self.write(':digitize')
+        
+        try:
+            if isinstance(channel, int):
+                waveform = self._get_waveform(channel)
+            else:
+                waveform = [self._get_waveform(ch) for ch in channel]
+        finally:
+            self.write(':run')
+        
+        return waveform
     
 
-    def set_y_offset(self, offset, channel):
-        if isinstance(channel, list):
-            for ch in channel:
-                self.write(f':channel{ch}:offset {offset}')
-        else:
-            self.write(f':channel{channel}:offset {offset}')
+    def set_sample_rate(self, fs:float|str) -> None:
+        # fs = AUTO | MAX | <rate>
+        self.write(f':acquire:srate {fs}')
     
 
-    def set_y_scale(self, scale, channel):
+    def get_sample_rate(self) -> float|None:
+        return self.query_value(':acquire:srate?')
+    
+    
+    def set_x_range(self, xrange:float) -> None:
+        # xrange is a real number for the horizontal time, in seconds.
+        # xrange in [50ps, 200s]
+        self.write(f':timebase:range {xrange}')
+    
+
+    def get_x_range(self) -> float|None:
+        return self.query_value(':timebase:range?')
+    
+    
+    def set_y_scale(self, scale:float, channel:int|List[int]) -> None:
         if isinstance(channel, list):
             for ch in channel:
                 self.write(f':channel{ch}:scale {scale}')
@@ -66,87 +81,74 @@ class DSO9104A(EIns):
             self.write(f':channel{channel}:scale {scale}')
     
 
-    def set_x_range(self, xrange):
-        # xrange is a real number for the horizontal time, in seconds.
-        # xrange in [50ps, 200s]
-        self.write(f':timebase:range {xrange}')
+    def get_y_scale(self, channel:int) -> float|None:
+        return self.query_value(f':channel{channel}:scale?')
+    
+    
+    def set_y_offset(self, offset:float, channel:int|List[int]) -> None:
+        if isinstance(channel, list):
+            for ch in channel:
+                self.write(f':channel{ch}:offset {offset}')
+        else:
+            self.write(f':channel{channel}:offset {offset}')
     
 
-    def set_sample_rate(self, fs):
-        # fs = AUTO | MAX | <rate>
-        self.write(f':acquire:srate {fs}')
+    def get_y_offset(self, channel:int) -> float|None:
+        return self.query_value(f':channel{channel}:offset?')
     
-
-    def get_sample_rate(self):
-        resp = self.query(':acquire:srate?').strip()
-        try:
-            return float(resp)
-        except ValueError as e:
-            print(f"Failed to convert response to float: {e}")
-            return None
     
-
-    def get_x_range(self):
-        resp = self.query(':timebase:range?').strip()
-        try:
-            return float(resp)
-        except ValueError as e:
-            print(f"Failed to convert response to float: {e}")
-            return None
-    
-
-    def get_y_scale(self, channel):
-        resp = self.query(f':channel{channel}:scale?').strip()
-        try:
-            return float(resp)
-        except ValueError as e:
-            print(f"Failed to convert response to float: {e}")
-            return None
-    
-
-    def get_y_offset(self, channel):
-        resp = self.query(f':channel{channel}:offset?').strip()
-        try:
-            return float(resp)
-        except ValueError as e:
-            print(f"Failed to convert response to float: {e}")
-            return None
-
-
-    def auto_set_y(self, channel, xrange=10e-3, Fs=1e6, ptp2div=2, y0=0, yscale0=1, N=2, yscalemin=20e-3):
+    def set_trigger_mode(self, mode:str) -> None:
         '''
-        auto tuning the y offset & range
-        Parameter:
-            channel: channel number or list of channel number
-            xrange: x range when tuning
-            Fs: Fs when tuing
-
-            ptp2div: parameter to determin the peak-to-peak value in DIV
-            y0: the initial value for y offset
-            yscale0: the initial value for y div
-            N: iterate for N times
+        mode = EDGE | GLITch | PATTern | STATe | DELay | TIMeout | TV | 
+            COMM | RUNT | SEQuence | SHOLd | TRANsition | WINDow | 
+            PWIDth | ADVanced | SBUS<N>
         '''
+        self.write(f':trigger:mode {mode}')
+    
+    
+    def get_trigger_mode(self) -> str:
+        return self.query(':trigger:mode?')
+    
+    
+    def set_trigger_level(self, channel:int, level:float) -> None:
+        self.write(f':trigger:level channel{channel}, {level}')
+    
+    
+    # TODO:
+    # def get_trigger_level(self, channel:int) -> float:
+    #     return 0
+    
+    
+    def set_acquire_mode(self, mode:str) -> None:
+        '''
+        mode = ETIMe | RTIMe | PDETect | HRESolution | SEGMented | SEGPdetect | SEGHres
+        '''
+        self.write(f':acquire:mode {mode}')
+    
+    
+    def get_acquire_mode(self) -> str:
+        return self.query(':acquire:mode?')
 
-        channel = channel if isinstance(channel,list) else [channel]
-
-        xrange_set = self.get_x_range()
-        Fs_set = self.get_sample_rate()
-
-        self.set_x_range(xrange)
-        self.set_sample_rate(Fs)
-
-        for ch in channel:
-            self.set_y_offset(y0, channel=ch)
-            self.set_y_scale(yscale0, channel=ch)
+    
+    def autoscale_vertical(self, channel: int) -> None:
+        self.write(f':autoscale:vertical channel{channel}')
+    
+    
+    def autoscale(self) -> None:
+        self.write(':autoscale')
         
-        for _ in range(N):
-            wvf = self.get_waveform(channel)
-            for ch,w in zip(channel,wvf):
-                yscale = np.ptp(w) / ptp2div
-                yscale = yscale if yscale > yscalemin else yscalemin
-
-                self.set_y_offset(np.min(w) + 0.5*np.ptp(w), channel=ch)
-                self.set_y_scale(yscale, channel=ch)
-        
-        self.set_x_range(xrange_set)
-        self.set_sample_rate(Fs_set)
+    
+    def run(self) -> None:
+        self.wrire(':run')
+    
+    
+    def stop(self) -> None:
+        self.write(':stop')
+    
+    
+    def single(self) -> None:
+        self.write(':single')
+    
+    
+    def digitize(self) -> None:
+        self.write(':digitize')
