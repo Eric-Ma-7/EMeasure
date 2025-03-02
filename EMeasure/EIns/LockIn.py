b06664e36fa96783abe0d58ba80701ed1f5dacec
Eric-Ma-7 <39188334+Eric-Ma-7@users.noreply.github.com>
2025-03-02 15:31:46 +0800

    Update LockIn.py

--- EMeasure/EIns/LockIn.py
+++ EMeasure/EIns/LockIn.py
@@ -1,26 +1,56 @@
 from .EIns import EIns
+from typing import List, Dict
+from enum import IntEnum
+
+
+class NF5650_STATUS(IntEnum):
+    NORM    = 0
+    PROTECT = 1
+    INPUT   = 2
+    OUTPUT  = 4
+    AUX     = 8
+    UNLOCK  = 16
+
 
 class NF5650(EIns):
     def __init__(self, resource_name):
         super().__init__(resource_name)
         
     
-    def set_sense_data(self, *data):
-        data_mapping = {'data1': 2, 'data2': 4, 'data3': 8, 'data4': 16}
+    def set_sense_data(self, *data) -> None:
+        data_mapping = {'status':1, 'data1': 2, 'data2': 4, 'data3': 8, 'data4': 16, 'freq':32}
         selected_data = sum(data_mapping[d.lower()] for d in data if d.lower() in data_mapping)
         if selected_data == 0:
             raise ValueError("At least one of data1, data2, data3, data4 must be specified")
-        command = f'SENS:DATA {selected_data}'
+        command = f'SENS:DATA {1 + selected_data}'
         self.write(command)
 
 
     def fetch_data(self):
-        command = 'FETCH?'
-        response = self.query(command)
+        response = self.query('FETCH?')
         data = response.strip().split(',')
-        try:
-            measured_data = [float(d) for d in data]
-            return measured_data
-        except ValueError as e:
-            print(f"Failed to parse measurement data: {e}")
-            return None
+        return tuple([float(d) for d in data])
+    
+    
+    def get_intosc_frequency(self) -> float:
+        return float(self.query(f':source:frequency?'))
+    
+    
+    def set_intosc_frequency(self, freq:float) -> None:
+        self.write(f':source:frequency {freq}')
+    
+    
+    def get_intosc_voltage(self) -> float:
+        return float(self.query(':source:voltage?'))
+    
+    
+    def set_intosc_voltage(self, voltage:float) -> None:
+        self.write(f':source:voltage {voltage}')
+    
+    
+    def get_intosc_voltage_range(self) -> float:
+        return float(self.query(':source:voltage:range'))
+    
+    
+    def set_intosc_voltage_range(self, vrange:float) -> None:
+        self.write(f':source:voltage:range {vrange}')
