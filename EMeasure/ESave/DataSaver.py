0c4a57164825013e905060c5a8eba8ca2a148b36
Eric-Ma-7 <39188334+Eric-Ma-7@users.noreply.github.com>
2025-03-04 18:06:37 +0800

    Update DataSaver.py

--- EMeasure/ESave/DataSaver.py
+++ EMeasure/ESave/DataSaver.py
@@ -1,53 +1,58 @@
-import queue
-import pandas as pd
-import os
-import datetime
+import numpy as np
+import h5py
+import threading
+from collections import defaultdict
 
-class DataSaver1D():
 
-    def __init__(self, columns, filename=''):
-        self._buffer = queue.Queue()
-        self._data = pd.DataFrame(data=None, columns=columns)
-        self._Ncol = len(columns)
-        self._file = filename + datetime.datetime.now().strftime("_%Y%m%d%H%M%S.csv")
+class DataSaver():
     
-
-    def __len__(self):
-        return len(self._data)
+    def __init__(self, 
+                 fname:str, 
+                 size:int|tuple[int], 
+                 default_val:np.integer|np.floating|np.complexfloating=np.nan) -> None:
+        self._lock = threading.Lock()
+        
+        self._fname = fname
+        self._size = size
+        self._defaul_val = default_val
+        
+        self._is_recorded = np.full(size, False, dtype=bool)
+        self._data = defaultdict(self._defaul_data)
     
-
-    def __getitem__(self, key):
-        return self._data[key].to_numpy()
-
-
-    def put(self, dataframe, **kwargs):
-        if len(dataframe) != self._Ncol:
-            raise ValueError(f'Length of dataframe {len(dataframe)} dose NOT match with the number of columns {self._Ncol}')
-        else:
-            self._buffer.put({
-                'dataframe': dataframe,
-                **kwargs
-            })
     
-
-    def get_one_from_buffer(self, save=True):
-        if not self._buffer.empty():
-            frame = self._buffer.get()
-            self._data.loc[len(self)] = frame.pop('dataframe')
-            if save:
-                self._data.to_csv(self._file, index=False)
-            return frame
-        else:
-            return None
-
-
-    def get_all_from_buffer(self, save=True):
-        while not self._buffer.empty():
-            frame = self._buffer.get()
-            self._data.loc[len(self)] = frame.pop('dataframe')
-        if save:
-            self._data.to_csv(self._file, index=False)
+    def _defaul_data(self) -> np.ndarray:
+        return np.full(self._size, self._defaul_val)
     
-
-    def is_buffer_empty(self):
-        return self._buffer.empty()
+    
+    @property
+    def shape(self):
+        return self._size
+    
+    
+    def keys(self):
+        with self._lock:
+            return self._data.keys()
+    
+    
+    def __getitem__(self, key) -> np.ndarray:
+        with self._lock:
+            return self._data[key]
+    
+    
+    def add(self, frame:dict, index:int|tuple[int]):
+        with self._lock:
+            self._is_recorded[index] = True
+            for key,val in frame.items():
+                self._data[key][index] = val
+    
+    
+    def save(self, nan:float=0, posinf:float=0, neginf:float=0):
+        with self._lock:
+            with h5py.File(self._fname, 'w') as f:
+                g_info = f.create_group('info')
+                g_info.create_dataset(name='is_recorded', data=self._is_recorded)
+                
+                for key,val in self._data.items():
+                    val = np.nan_to_num(val, nan=nan, posinf=posinf, neginf=neginf)
+                    f.create_dataset(name=key, data=val)
+    
