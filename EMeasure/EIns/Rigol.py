9c081f5dfed544dfa3f5e35bb41b2aa201534d0d
Eric-Ma-7 <39188334+Eric-Ma-7@users.noreply.github.com>
2025-03-05 12:46:43 +0800

    Update Rigol.py
    add methods about controlling the voltage and frequency

--- EMeasure/EIns/Rigol.py
+++ EMeasure/EIns/Rigol.py
@@ -26,11 +26,18 @@ class DG1022(EIns):
                     return None
 
 
-    def get_waveform_info(self, channel:int=1) -> dict:
+    def _header_add_channel(self, cmd:str, channel:int=1) -> str:
         if channel == 1:
-            resp = self.query('apply?').strip()
+            return cmd
+        elif channel == 2:
+            return cmd + ':ch2'
         else:
-            resp = self.query('apply:ch2?').strip()
+            raise ValueError(f'Channel is expected to be 1 or 2, but {channel} is received.')
+
+
+    def get_waveform_info(self, channel:int=1) -> dict:
+        header = self._header_add_channel('apply', channel)
+        resp = self.query(f'{header}?').strip()
         resp = resp[len("CHx:'"):-1].split(',')
         return {
             'type': resp[0],
@@ -46,28 +53,85 @@ class DG1022(EIns):
                      offset:float, 
                      channel:int=1) -> None:
         
-        if channel == 1:
-            self.write(f'apply:{type} {frequency},{amplitude},{offset}')
-        elif channel == 2:
-            self.write(f'apply:{type}:ch2 {frequency},{amplitude},{offset}')
-        else:
-            raise ValueError(f'Channel is expected to be 1 or 2, but {channel} is received.')
+        header = self._header_add_channel(f'apply:{type}', channel)
+        self.write(f'{header} {frequency},{amplitude},{offset}')
 
 
-
-    def get_output_state(self, channel:int=1):
-        if channel == 1:
-            return self.query(f'output?')
-        elif channel == 2:
-            return self.query(f'output:ch2?')
-        else:
-            raise ValueError(f'Channel is expected to be 1 or 2, but {channel} is received.')
+    def get_output_state(self, channel:int=1) -> str:
+        header = self._header_add_channel('output', channel)
+        return self.query(f'{header}?')
     
 
-    def set_output_state(self, state, channel:int=1):
-        if channel == 1:
-            return self.write(f'output {state}')
-        elif channel == 2:
-            return self.write(f'output:ch2 {state}')
-        else:
-            raise ValueError(f'Channel is expected to be 1 or 2, but {channel} is received.')
+    def set_output_state(self, state, channel:int=1) -> None:
+        header = self._header_add_channel('output', channel)
+        self.write(f'{header} {state}')
+    
+    
+    def get_voltage_amplitude(self, channel:int=1) -> float:
+        header = self._header_add_channel('voltage', channel)
+        return float(self.query(f'{header}?'))
+    
+    
+    def set_voltage_amplitude(self, amplitude:float, channel:int=1) -> None:
+        header = self._header_add_channel('voltage', channel)
+        self.write(f'{header} {amplitude}')
+    
+    
+    def get_voltage_offset(self, channel:int=1) -> float:
+        header = self._header_add_channel('voltage:offset', channel)
+        return float(self.query(f'{header}?'))
+    
+    
+    def set_voltage_offset(self, offset:float, channel:int=1) -> None:
+        header = self._header_add_channel('voltage:offset', channel)
+        self.write(f'{header} {offset}')
+    
+    
+    def get_voltage_high(self, channel:int=1) -> float:
+        header = self._header_add_channel('voltage:high', channel)
+        return float(self.query(f'{header}?'))
+    
+    
+    def set_voltage_high(self, voltage_high:float, channel:int=1) -> None:
+        header = self._header_add_channel('voltage:high', channel)
+        self.write(f'{header} {voltage_high}')
+    
+    
+    def get_voltage_low(self, channel:int=1) -> float:
+        header = self._header_add_channel('voltage:low', channel)
+        return float(self.query(f'{header}?'))
+    
+    
+    def set_voltage_low(self, voltage_low:float, channel:int=1) -> None:
+        header = self._header_add_channel('voltage:low', channel)
+        self.write(f'{header} {voltage_low}')
+    
+    
+    def get_voltage_unit(self, channel:int=1) -> str:
+        '''
+        unit = VPP | VRMS | DBM
+        '''
+        header = self._header_add_channel('voltage:unit', channel)
+        return self.query(f'{header}?')
+    
+    
+    def set_voltage_unit(self, unit:str, channel:int=1) -> None:
+        '''
+        unit = VPP | VRMS | DBM
+        '''
+        header = self._header_add_channel('voltage:unit', channel)
+        self.write(f'{header} {unit}')
+    
+    
+    def get_frequency(self, channel:int=1) -> float:
+        header = self._header_add_channel('frequency', channel)
+        return float(self.query(f'{header}?'))
+    
+    
+    def set_frequency(self, frequency:float, channel:int=1) -> None:
+        header = self._header_add_channel('frequency', channel)
+        self.write(f'{header} {frequency}')
+
+    
+    def set_phase_align(self) -> None:
+        self.write('phase:align')
