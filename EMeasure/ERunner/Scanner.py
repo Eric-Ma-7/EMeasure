1fe1f79a81d76dd3ebba5c754600345178b18e91
Eric-Ma-7 <39188334+Eric-Ma-7@users.noreply.github.com>
2025-03-05 12:46:54 +0800

    Update Scanner.py
    Add Scanner2D

--- EMeasure/ERunner/Scanner.py
+++ EMeasure/ERunner/Scanner.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
-import numpy as np
+from enum import StrEnum, auto
 from typing import Generator
 from collections import namedtuple
+
 import time
-from enum import StrEnum, auto
+import numpy as np
+
 
 
 class SCAN_MODE(StrEnum):
@@ -85,6 +87,84 @@ class Scanner1D(ABC):
     def __len__(self) -> int:
         return len(self.param)
     
+    
+    def __enter__(self):
+        self.initializer()
+        return self.loop()
+    
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.finalizer()
+
+
+
+class Scanner2D(ABC):
+    
+    def __init__(self, 
+                 outer:dict[list|np.ndarray], 
+                 inner:dict[list|np.ndarray],
+                 outer_mode:SCAN_MODE=SCAN_MODE.FORWARD,
+                 inner_mode:SCAN_MODE=SCAN_MODE.FORWARD) -> None:
+        
+        super().__init__()
+        
+        self.outer = ScanTools.reshape_param(
+            param = ScanTools.dict2list(outer), 
+            mode = outer_mode
+        )
+        
+        self.inner = ScanTools.reshape_param(
+            param = ScanTools.dict2list(inner),
+            mode = inner_mode
+        )
+        
+
+    @abstractmethod
+    def initializer(self):
+        pass
+    
+    
+    @abstractmethod
+    def finalizer(self):
+        pass
+    
+    
+    @abstractmethod
+    def setter_inner(self, param_to_set:dict):
+        pass
+    
+    
+    @abstractmethod
+    def setter_outer(self, param_to_set:dict):
+        pass
+    
+    
+    @abstractmethod
+    def getter(self):
+        pass
+    
+    
+    def loop(self) -> Generator:
+        for i in range(len(self.outer)):
+            p_outer = self.outer[i]
+            self.setter_outer(p_outer)
+            
+            for j in range(len(self.inner)):
+                p_inner = self.inner[j]
+                self.setter_inner(p_inner)
+                
+                frame = self.getter()
+                yield (i, j), {**p_outer, **p_inner}, frame
+    
+    
+    @property
+    def shape(self) -> tuple[int]:
+        return (len(self.outer), len(self.inner))
+    
+    
+    def __len__(self) -> int:
+        return len(self.outer) * len(self.inner)
+
     
     def __enter__(self):
         self.initializer()
