fbef98c3c1914eed11541a486a520461b4a9ebbd
Eric-Ma-7 <39188334+Eric-Ma-7@users.noreply.github.com>
2025-03-12 16:47:57 +0800

    Update Keithley.py

--- EMeasure/EIns/Keithley.py
+++ EMeasure/EIns/Keithley.py
@@ -1,55 +1,39 @@
+from .EIns import EIns
+
 import time
 import numpy as np
-from .EIns import EIns
+
 
 
 class K2612(EIns):
 
-    def __init__(self, resource_name):
+    def __init__(self, resource_name:str) -> None:
         super().__init__(resource_name)
     
 
-    def set_voltage(self, voltage, channel):
-        command = f'smu{channel}.source.levelv = {voltage}'
-        self.write(command)
+    def set_voltage(self, voltage:float, channel:str) -> None:
+        self.write(f'smu{channel}.source.levelv = {voltage}')
     
 
-    def set_current(self, current, channel):
-        command = f'smu{channel}.source.leveli = {current}'
-        self.write(command)
+    def set_current(self, current:float, channel:str) -> None:
+        self.write(f'smu{channel}.source.leveli = {current}')
     
 
-    def get_current(self, channel):
-        command = f'print(smu{channel}.measure.i())'
-        response = self.query(command)
-        try:
-            current = float(response.strip())
-            return current
-        except ValueError as e:
-            print(f'Failed to conver response to float: {e}')
-            return None
+    def get_current(self, channel:str) -> float:
+        response = self.query(f'print(smu{channel}.measure.i())')
+        return float(response.strip())
     
 
-    def get_voltage(self, channel):
-        command = f"print(smu{channel}.measure.v())"
-        response = self.query(command)
-        try:
-            voltage = float(response.strip())
-            return voltage
-        except ValueError as e:
-            print(f"Failed to convert response to float: {e}")
-            return None
+    def get_voltage(self, channel:str) -> float:
+        response = self.query(f"print(smu{channel}.measure.v())")
+        return float(response.strip())
 
 
-    def get_iv(self, channel):
-        command = f"print(smu{channel}.measure.iv())"
-        response = self.query(command)
-        try:
-            iv = [float(x) for x in response.split()]
-            return tuple(iv)
-        except ValueError as e:
-            print(f"Failed to convert response to float: {e}")
-            return None
+    def get_iv(self, channel:str) -> tuple[float]:
+        response = self.query(f"print(smu{channel}.measure.iv())")
+        iv = [float(x) for x in response.split()]
+        return tuple(iv)
+    
 
 
     def set_current_ramp(self, current, dI, dt, channel):
@@ -78,7 +62,7 @@ class K2612(EIns):
         self.set_voltage(voltage, channel=channel)
 
 
-    def set_output_state(self, state, channel):
+    def set_output_state(self, state:str, channel:str) -> None:
         if state.lower() not in ['on','off']:
             raise ValueError("State must be 'on' or 'off'")
         state_command = '1' if state.lower() == 'on' else '0'
@@ -86,27 +70,27 @@ class K2612(EIns):
         self.write(command)
     
 
-    def set_voltage_limit(self, voltage_limit, channel):
+    def set_voltage_limit(self, voltage_limit:float, channel:str) -> None:
         command = f"smu{channel}.source.limitv = {voltage_limit}"
         self.write(command)
     
 
-    def set_current_limit(self, current_limit, channel):
+    def set_current_limit(self, current_limit:float, channel:str) -> None:
         command = f"smu{channel}.source.limiti = {current_limit}"
         self.write(command)
 
 
-    def set_current_range(self, current_range, channel):
+    def set_current_range(self, current_range:float, channel:str) -> None:
         command = f"smu{channel}.source.rangei = {current_range}"
         self.write(command)
 
 
-    def set_voltage_range(self, voltage_range, channel):
+    def set_voltage_range(self, voltage_range:float, channel:str) -> None:
         command = f"smu{channel}.source.rangev = {voltage_range}"
         self.write(command)
 
 
-    def set_measurement_mode(self, measurement_mode, channel):
+    def set_measurement_mode(self, measurement_mode:str, channel:str) -> None:
         if measurement_mode.lower() not in ['2-wire', '4-wire']:
             raise ValueError("Measurement mode must be '2-wire' or '4-wire'")
         if measurement_mode.lower() == '4-wire':
@@ -116,7 +100,7 @@ class K2612(EIns):
         self.write(command)
 
 
-    def set_mode(self, mode, channel):
+    def set_mode(self, mode:str, channel:str) -> None:
         if mode.lower() not in ['current', 'voltage']:
             raise ValueError("Mode must be 'current' or 'voltage'")
         if mode.lower() == 'current':
@@ -124,21 +108,18 @@ class K2612(EIns):
         else:
             command = f"smu{channel}.source.func = smu{channel}.OUTPUT_DCVOLTS"
         self.write(command)
+    
+    
+    
 
 
 
 class K2182(EIns):
 
-    def __init__(self, resource_name):
+    def __init__(self, resource_name:str) -> None:
         super().__init__(resource_name)
     
     
     def get_data(self):
-        command = ":fetch?"
-        response = self.query(command)
-        try:
-            data = float(response.strip())
-            return data
-        except ValueError as e:
-            print(f"Failed to convert response to float: {e}")
-            return None
+        response = self.query(":fetch?")
+        return float(response.strip())
